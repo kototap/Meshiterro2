b42a71b3f3667c62ad0a20fbea455de474394b98
Kototap <kotonesmz31@gmail.com>
2022-11-20 06:36:25 +0000

    [Add]削除リンク

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -16,7 +16,12 @@ class PostImagesController < ApplicationController
 
   def show
     @post_image = PostImage.find(params[:id])
-    
+  end
+  
+  def destroy
+    @post_image = PostImage.destroy(params[:id])
+    @post_image.destroy
+    redirect_to post_images_path
   end
   
   

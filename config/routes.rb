d12da2909cfbf62c0e22e4404f1777e626414488
Kototap <kotonesmz31@gmail.com>
2022-11-20 07:35:43 +0000

    [Add]プロフィール編集

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
 
-  resources :users, only: [:edit, :show]
+  resources :users, only: [:edit, :show, :update]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
